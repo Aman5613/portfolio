08e973d0f3afeef29538d51b34950061f42fbc22
Aman5613 <161672923+Aman5613@users.noreply.github.com>
2025-07-19 01:24:12 +0530

    intial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,112 @@
+import React, { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
+import { BiLogoGmail } from 'react-icons/bi';
+import { BsGithub } from 'react-icons/bs';
+import { IoLogoLinkedin, IoLogoTwitter } from 'react-icons/io5';
+import { IoMdMail } from "react-icons/io";
+import { FaPhone } from "react-icons/fa6";
+import { icons_links } from '..';
+
+export default function Contact() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.2 });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0 }}
+      animate={isInView ? { opacity: 1 } : { opacity: 0 }}
+      transition={{ duration: 0.8 }}
+      className='lg:my-16 lg:px-28 my-8 px-5'
+      id='contact'
+    >
+      <motion.h2
+        initial={{ y: -50, opacity: 0 }}
+        animate={isInView ? { y: 0, opacity: 1 } : { opacity: 0 }}
+        transition={{ duration: 0.8 }}
+        className='text-2xl lg:text-4xl text-center'
+      >
+        Contact <span className='font-extrabold'>Me</span>
+      </motion.h2>
+
+      <div className='flex justify-between  mt-8 lg:mt-16 flex-col lg:flex-row'>
+        <motion.div
+          initial={{ x: -50, opacity: 0 }}
+          animate={isInView ? { x: 0, opacity: 1 } : { opacity: 0 }}
+          transition={{ duration: 0.8 }}
+          className='lg:w-[40%]'
+        >
+
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0 }}
+              transition={{ duration: 0.8 }}
+              className='flex justify-between gap-3 lg:gap-5 flex-col lg:flex-row'
+            >
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                // type='submit'
+                className='bg-black justify-center w-fit lg:w-auto lg:flex-1 hover:shadow-lg text-white px-3 py-2 rounded flex items-center gap-x-3 font-medium'
+              >
+                Get In Touch
+              </motion.button>
+
+              <div className='flex items-center gap-x-2 lg:gap-x-5'>
+                {icons_links.map((Icon, index) => (
+                  <motion.a
+                    key={index}
+                    href={Icon.link}
+                    className="bg-white p-2 lg:p-3 rounded border-2 border-black"
+                    whileHover={{ scale: 1.1, backgroundColor: "#000", color: "#fff" }}
+                    whileTap={{ scale: 0.9 }}
+                  >
+                    <Icon.icon className="w-4 h-4 lg:w-5 lg:h-5" />
+                  </motion.a>
+                ))}
+              </div>
+            </motion.div>
+          
+        </motion.div>
+
+        <motion.div
+          initial={{ x: 50, opacity: 0 }}
+          animate={isInView ? { x: 0, opacity: 1 } : { opacity: 0 }}
+          transition={{ duration: 0.8 }}
+          className='lg:w-1/2'
+        >
+          <div className='font-extrabold text-2xl lg:text-5xl mt-5 lg:mt-0 space-y-1 lg:space-y-3'>
+            <h2>Let's <span className='text-white' style={{ WebkitTextStroke: '1px black' }}>talk</span> for</h2>
+            <h2>Something special</h2>
+          </div>
+
+          <p className='text-[#71717A] text-sm/6 lg:text-base mt-3 lg:mt-6'>I seek to push the limits of creativity to create high-engaging, user-friendly, and memorable interactive experiences.</p>
+
+          <div className='font-semibold text-sm lg:text-xl flex flex-col mt-6 gap-2 lg:gap-4'>
+            <motion.a
+              whileHover={{ x: 5 }}
+              className='flex items-center gap-2 group'
+              href="mailto:connect2amangupta@gmail.com?subject=Hellooo%20Aman%2C%20I%20found%20your%20portfolio!&body=Hi%20aman%2C%0D%0AI%20found%20across%20your%20portfolio%20and%20wanted%20to%20reach%20out.%0D%0AI'm%20%7Byour%20name%7D%2C%0D%0A%7Bwrite%20your%20message%20for%20aman%7D"
+            >
+              <span className='border-2 transition-all border-transparent group-hover:border-black rounded-full p-1'>
+                <IoMdMail className="w-4 h-4 lg:w-5 lg:h-5" />
+              </span>
+              connect2amangupta
+            </motion.a>
+
+            <motion.a
+              whileHover={{ x: 5 }}
+              className='flex items-center gap-2 group'
+              href="tel:8738038451"
+              target='_blank'
+            >
+              <span className='border-2 transition-all border-transparent group-hover:border-black rounded-full p-[5px]'>
+                <FaPhone className="w-3 h-3 lg:w-4 lg:h-4" />
+              </span>
+              8738038451
+            </motion.a>
+          </div>
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,123 @@
+
+import { IoLogoLinkedin, IoLogoTwitter } from "react-icons/io5";
+import { BiLogoGmail } from "react-icons/bi";
+import { BsGithub, BsInstagram, BsWhatsapp } from "react-icons/bs";
+import { SiExpress, SiLeetcode } from "react-icons/si";
+import { DiCss3, DiGithub, DiGithubAlt, DiHtml5, DiJava, DiJavascript, DiMongodb, DiPython, DiReact } from "react-icons/di";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { CgFramer } from "react-icons/cg";
+import { FiGithub } from "react-icons/fi";
+import { ImGithub } from "react-icons/im";
+import { FaJava } from "react-icons/fa6";
+import { FaDatabase } from "react-icons/fa";
+
+export const icons_links = [
+    {
+        icon: IoLogoLinkedin,
+        link: "https://www.linkedin.com/in/amankumarguptabeginner/"
+    },
+    {
+        icon: IoLogoTwitter,
+        link: "https://x.com/aman3779956"
+    },
+    {
+        icon: BiLogoGmail,
+        link: "mailto:connect2amangupta@gmail.com?subject=Hellooo%20Aman%2C%20I%20found%20your%20portfolio!&body=Hi%20aman%2C%0D%0AI%20found%20across%20your%20portfolio%20and%20wanted%20to%20reach%20out.%0D%0AI'm%20%7Byour%20name%7D%2C%0D%0A%7Bwrite%20your%20message%20for%20aman%7D"
+    },
+    {
+        icon: BsGithub,
+        link: "https://github.com/Aman5613"
+    },
+    {
+        icon: SiLeetcode,
+        link: "https://leetcode.com/u/Aman_5613/"
+    },
+    {
+        icon: BsWhatsapp,
+        link: "https://api.whatsapp.com/send?phone=918738038451"
+    },
+    {
+        icon: BsInstagram,
+        link: "https://instagram.com/amangupta_5613"
+    },
+]
+
+export const skills = [
+    {
+        icon: DiJavascript,
+        name: "JavaScript"
+    },
+    {
+        icon: DiReact,
+        name: "React"
+    },
+    {
+        icon: DiHtml5,
+        name: "HTML"
+    },
+    {
+        icon: DiCss3,
+        name: "CSS"
+    },
+    {
+        icon: RiTailwindCssFill,
+        name: "Tailwind"
+    },
+    {
+        icon: ImGithub,
+        name: "Github"
+    },
+    {
+        icon: CgFramer,
+        name: "Framer"
+    },
+    {
+        icon: FaJava,
+        name: "Java"
+    },
+    {
+        icon: DiPython,
+        name: "Python"
+    },
+    {
+        icon: FaDatabase,
+        name: "MongoDB"
+    },
+    {
+        icon: SiExpress,
+        name: "Express"
+    }
+]
+
+export const projects = [
+    {
+        title: "Fylla - Digital Agency",
+        description: "A powerful tool for tracking and analyzing cryptocurrency trends in real-time.",
+        image: "assets/project-fylla.png",
+        link: "https://aman5613.github.io/FYLLA/"
+    },
+    {
+        tittle: "Arik Web Designer",
+        description: "A powerful tool for tracking and analyzing cryptocurrency trends in real-time.",
+        image: "assets/project-arik.png",
+        link: "https://aman5613.github.io/arik/"
+    },
+    {
+        tittle : "Protffolio",
+        description: "A powerful tool for tracking and analyzing cryptocurrency trends in real-time.",
+        image : "assets/project-portfolio.png",
+        link : "https://aman5613.github.io/portf/"
+    },
+    {
+        tittle : "Lazarev",
+        description: "A powerful tool for tracking and analyzing cryptocurrency trends in real-time.",
+        image : "assets/project-lazarev.png",
+        link : "https://aman5613.github.io/design-agency/"
+    },
+    {
+        tittle : "Cynthia Ugwa - Product Designer",
+        description: "A powerful tool for tracking and analyzing cryptocurrency trends in real-time.",
+        image : "assets/project-cynthia.png",
+        link : "https://aman5613.github.io/Product-Designer/"
+    },
+]
